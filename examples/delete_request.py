886f35d213c8cefc1f03bcae9d531b87dac72aad
mcarlton00 <mcarlton00@gmail.com>
2020-01-09 20:48:41 -0500

    Add example delete request

---
+++ examples/delete_request.py
@@ -0,0 +1,24 @@
+from logicmonitor import LM
+from pprint import pprint
+
+"""
+This script will delete the dashboard with an id of 41
+"""
+
+# Authentication Information
+account_name='acme-firework-co',
+access_id='Fahpho7Pahghahwo1aeY',
+access_key='=oS-iiKuhei5Ahth{e(oph[ei8Ow8pheekai9iGh'
+
+# Initialize your API connections
+lm = LM(account_name, access_id, access_key)
+
+# Delete the dashboard at id 41
+response = lm.delete('/dashboard/dashboards/41')
+
+# PrettyPrint the result
+pprint(response)
+
+"""
+{'data': None, 'errmsg': 'OK', 'status': 200}
+"""
